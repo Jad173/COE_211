919d0f995245b799b63cb2f69ecaf02449c2096b
Jad173 <83554381+Jad173@users.noreply.github.com>
2022-02-21 20:15:41 +0200

    Add files via upload

---
+++ StaffManagement/Employee.java
@@ -0,0 +1,18 @@
+public class Employee
+{		
+	String firstName;
+	String lastName;
+	int age;
+	double monthlySalary;
+	
+	public Employee(String firstName, String lastName, int age, double monthlySalary)
+	{
+		
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+			this.monthlySalary = monthlySalary;
+		
+	}
+
+}		
